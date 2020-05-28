588308b976c00126f37a1ddad4493d31b4cf9014
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-05-28 15:52:10 +0200

    Clean up unused types

--- packages/core/src/shared/types.ts
+++ packages/core/src/shared/types.ts
@@ -4,14 +4,7 @@ export type ID = string;
 export type Nil = undefined | null;
 export type Maybe<T> = T | Nil;
 export type Plural<T> = T | T[];
-export type PromiseOrValue<T> = Promise<T> | T;
 export type Single<T> = T extends Array<infer R> ? R : T;
-export type Tuple<T1, T2> = [T1, T2];
-export type Fallback<T, F> = T extends never ? F : T;
-export type Dictionary<T, R = never> = {
-  [P in keyof T]: Fallback<R, T[P]>;
-};
-export type ValuesOf<T> = T[keyof T];
 export type ResolveFn<TContext> = (
   parent: any,
   args: Record<string, any>,
